c8c8cc90bbf94421348e933e3f50c0c5f6421c06
DYLAN SUN <zhangjie.sun@mail.utoronto.ca>
2023-09-18 22:35:12 -0400

    Implement: email field with validator

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, StopValidation
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -13,6 +13,12 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+
+    def validate_email(form, field):
+        if '@' not in field.data:
+            raise StopValidation(f"Please include an '@' in the email address. '{field.data}' is missing an '@'")
+
+    email = StringField('What is your UofT Email Address?', validators=[DataRequired(), validate_email])
     submit = SubmitField('Submit')
 
 
@@ -31,11 +37,15 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
